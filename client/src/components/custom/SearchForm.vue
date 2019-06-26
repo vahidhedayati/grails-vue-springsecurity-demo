e1cc479f358dce3eef8b0bf0578409cc9a3d5efd
vahid <badvad@gmail.com>
2019-06-26 10:45:31 +0100

    ajax autocomplete sample added from micronaut-vuejs-cqrs

--- client/src/components/custom/SearchForm.vue
+++ client/src/components/custom/SearchForm.vue
@@ -10,7 +10,14 @@
       </div>
       <div class="col-sm-2">
         <div class="input-group">
-          <input type="text" class="form-control" placeholder="vehicleName" v-model.trim="search.vehicleName">
+          <!-- input type="text" class="form-control" placeholder="vehicleName" v-model.trim="search.vehicleName" -->
+           <autocomplete form-field="search"
+                                 @key-press="updateAutoCompleteItems"
+                                 @search-value="updateSearchValue"
+                                 @search-key="updateSearchKey"
+                                 key-field="id" value-field="vehicleName"
+                                 :items="vehicles" />
+
         </div>
       </div>
       <div class="col-sm-8">
@@ -51,10 +58,13 @@
   </div>
 </template>
 <script>
+import $ from 'jquery';
 import FieldSelect from '../form/FieldSelect';
 import Datepicker from 'vuejs-datepicker';
+import Autocomplete from '../Autocomplete'
 import moment from 'moment';
 import TestModal from './TestModal';
+import GarageService from '@/services/GarageService'
 export default {
   props: ['search','contract' ,'makes', 'models', 'drivers','contracts'],
 
@@ -68,15 +78,47 @@ export default {
     FieldSelect,
     Datepicker,
     moment,
-    TestModal
+    TestModal,
+    Autocomplete,
+    GarageService
   },
   data: function () {
     return {
       showModal:false,
+      vehicles:[]
     }
 
   },
   methods: {
+   updateSearchValue: function (value) {
+     this.search.vehicleName=value
+    },
+    updateSearchKey: function (key) {
+     //this.hotel.updateUserId=key
+    },
+    updateAutoCompleteItems: function (searchValue) {
+      if (searchValue.length>2) {
+        this.users=[];
+        var variables = $.param(searchValue);
+        variables+="&max=10&offset=0";
+        this.initialiseVehicles(variables);
+      }
+    },
+    initialiseVehicles(params){
+       return GarageService.fetchName('customRest?'+params)
+       .then((res) => {
+        if (res) {
+          if (res.data.instanceList) {
+            this.vehicles = res.data.instanceList;
+          } else {
+            if (res.data) {
+              this.vehicles = res.data;
+
+            }
+          }
+        }
+       });
+    },
     submit () {
       this.$emit('submit')
     },
